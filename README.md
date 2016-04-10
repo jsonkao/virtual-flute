348076ca25dd17b23443e5f56c9c5b64f4e53c6f
Jason Kao <jkao1@stuy.edu>
2016-04-10 17:17:04 -0400

    moved repo, continuing

---
+++ README.md
@@ -0,0 +1,33 @@
+# virtual-flute
+
+### Overview
+My experience as a floutist and my passion for computer science have joined to form the Virtual Flute. This program, 
+written as a web app, let's the user play the flute using matching keys (position-wise) on the keyboard as buttons on a flute. 
+
+### Usage
+First, add the js:
+```html
+<script src="vflute.js"></script>
+```
+Then, reference all necessary audio files using the file name as the tag id:
+```html
+<audio id="m-D"><source src="m-D"></audio>
+<audio id="m-E"><source src="m-E"></audio>
+<audio id="m-D"><source src="m-F"></audio>
+<audio id="m-D"><source src="m-G"></audio>
+<audio id="m-E"><source src="m-A"></audio>
+...
+```
+### Key Documentation
+A diagram of terms I will be using in this documentation:
+<br>
+<img src="key_diagram.jpg"/><br>
+TH - space<br>
+LH1 - F<br>
+LH2 - D<br>
+LH3 - S<br>
+LH4 - A<br>
+RH1 - J<br>
+RH2 - K<br>
+RH3 - L<br>
+more soon to come...
---
+++ vflute.js
@@ -0,0 +1,113 @@
+/* Canvas */
+var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    W,H,
+    key = [];
+canvas.tabIndex = 0;
+canvas.width = W = 600;
+canvas.height = H = 60;
+canvas.style.position = "relative";
+canvas.style.left = "40%";
+canvas.style.border = '1px solid #1abc9c';
+ctx.font = 'bold 12px Open Sans';
+ctx.textBaseline = 'top';
+ctx.textAlign = 'center';
+document.body.appendChild(canvas);
+canvas.focus();
+document.body.onmouseover = function(){
+    canvas.focus();
+}
+
+var playing = []
+
+/* 'keydown' and 'keyup' events */
+canvas.onkeydown = canvas.onkeyup = function(e) {
+    var e = e || event;
+    key[e.keyCode] = e.type == 'keydown';
+    if (e.type == 'keyup') {
+        for (var x in playing) {
+            pauseAudio(playing[x]);
+        }
+    }
+};  
+
+/* Play/Pause Audio  */
+function playAudio(audio) { 
+    if (!(playing[0] == audio)) {
+        playing.push(audio);
+    }
+    for (var x in playing){
+        if (playing[x] != audio) {
+            pauseAudio(playing[x]);
+        }
+    }
+    document.getElementById(audio).play(); 
+    
+} 
+function pauseAudio(audio) {
+    document.getElementById(audio).pause();
+}   
+
+/* Images */
+
+/* innerHTML */
+(function loop(){
+    ctx.clearRect(0,0,W,H);
+    var y = 0, l = key.length, i, t;
+    for (i = 0; i < l; i ++) {
+        function keyFingering(n) {
+            /* elem's in n must be in the same order as keys in keys */
+            var keys = {
+                70:'key[70]', // left1 (F, 70) 
+                68:'key[68]', // left2 (D, 68) 
+                83:'key[83]', // left3 (S, 83) 
+                65:'key[65]', // Ab key (A, 65) 
+                74:'key[74]', // right1 (J, 74) 
+                75:'key[75]', // right2 (K, 75) 
+                76:'key[76]', // right3 (L, 76) 
+                59: 'key[59]' // Eb key (;, 59) 
+            }
+            var output = '';
+            for (var x in n) {
+                output += keys[n[x]] + '&&'
+            }
+            return eval(output + 'true') && true;
+        }
+        var thumb = key[i] && i == 32;
+        /* notes */
+        if (thumb&&keyFingering([68,83,74,75,76])) { 
+            t = 'm-D';
+            playAudio(t);
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68,83,74,75])) { 
+            t = 'm-E';
+            playAudio(t);
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68,83,74])) { 
+            t = 'm-F';
+            playAudio(t);
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68,83,76])) { 
+            t = 'm-F#Gb';
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68,83,65])) { 
+            t = 'm-G#/Ab';
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68,83])) { 
+            t = 'm-G';
+            playAudio(t);
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,68])) { 
+            t = 'm-A';
+            playAudio(t);
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70,74])) { 
+            t = 'm-A#Bb';
+            ctx.fillText(t,50,10);
+        } else if (thumb&&keyFingering([70])) { 
+            t = 'm-B';
+            ctx.fillText(t,50,10);
+        }
+    }
+    setTimeout(loop,1000/24);
+})();
